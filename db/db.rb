c6f31788811d0a23c8f59aeb1cbe2baec96249f9
Geoph88 <geoffrey_sizgoric@hotmail.com>
2022-05-19 10:29:31 +1000

    attempt to fix database error

--- db/db.rb
+++ db/db.rb
@@ -1,8 +1,8 @@
 require 'pg'
 
 def run_sql(sql, params=[])
-    db = PG.connect(dbname: 'cb_media')
+    db = PG.connect(ENV['DATABASE_URL'] || {dbname: 'cb_media'})
     data = db.exec_params(sql, params)
     db.close
     data
-  end
+end
